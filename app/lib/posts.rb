aae7a629eac97b2b601ff60f8a6300fcb150bfae
Ali Craigmile <ali@craigmile.com>
2015-02-24 15:29:34 +0000

    fix for posts

--- app/lib/posts.rb
+++ app/lib/posts.rb
@@ -2,7 +2,7 @@ require 'active_record'
 
 ActiveRecord::Base.establish_connection(
   :adapter => 'sqlite3',
-  :database =>  '../curriculum-data.sqlite3.db'
+  :database =>  'curriculum-data.sqlite3.db'
 )
 
 class Post < ActiveRecord::Base
