a4a2811f5e88938cd28891d66c74872fa2eae3b1
Ali Craigmile <ali@craigmile.com>
2015-02-24 07:53:55 +0000

    added conneg on /hello endpoint

--- app/app.rb
+++ app/app.rb
@@ -1,11 +1,40 @@
 require 'rubygems'
-require 'sinatra'
+libs = File.expand_path("vendor/bundle/gems/**/lib", __FILE__)
+$LOAD_PATH.unshift *Dir.glob(libs)
+$LOAD_PATH.unshift File.dirname(__FILE__)
 
-$:.unshift File.dirname(__FILE__)
+require 'sinatra'
+require 'rack/conneg'
+
 require 'posts'
-     
+
+use(Rack::Conneg) { |conneg|
+  conneg.set :accept_all_extensions, false
+  conneg.set :fallback, :html
+  conneg.ignore_contents_of(File.join(File.dirname(__FILE__),'public'))
+  conneg.provide([:json, :xml])
+}
+ 
+before do
+  if negotiated?
+    content_type negotiated_type
+  end
+end
+    
 get "/" do
     erb :apidocs
+end
+
+get '/hello' do
+  response = { :message => 'Hello, World!' }
+  respond_to do |wants|
+    wants.json  { response.to_json         }
+    wants.xml   { response.to_xml          }
+    wants.other { 
+      content_type 'text/plain'
+      error 406, "Not Acceptable" 
+    }
+  end
 end
 
 get "/ping" do
