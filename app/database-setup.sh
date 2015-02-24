d456fbba1b321ec927581f3fc0fed7629d337781
Ali Craigmile <ali@craigmile.com>
2015-02-24 00:19:03 +0000

    posts now include created and updated times, and a body.

--- app/database-setup.sh
+++ app/database-setup.sh
@@ -7,8 +7,21 @@ DROP TABLE IF EXISTS posts;
 EOT
 
 sqlite3 $DATABASE << EOT
-CREATE TABLE posts (id INTEGER PRIMARY KEY, title varchar(255));
-INSERT INTO posts VALUES ('1', 'A post');
-INSERT INTO posts VALUES ('2', 'Another post');
-INSERT INTO posts VALUES ('3', 'A third post');
+CREATE TABLE posts (
+    id INTEGER PRIMARY KEY,
+    title varchar(255),
+    body blob,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP
+);
+
+CREATE TRIGGER your_table_trig AFTER UPDATE ON posts
+BEGIN
+    update posts SET updated_at = datetime('now') WHERE id = NEW.id;
+END;
+
+INSERT INTO posts ('id','title','body') VALUES ('1', 'A post','Blah...');
+INSERT INTO posts ('id','title','body') VALUES ('2', 'Another post','Foo bar. Farp.');
+INSERT INTO posts ('id','title','body') VALUES ('3', 'A third post','Blah...');
+
 EOT
