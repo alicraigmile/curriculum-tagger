43d39206953dc87a856f0ff3c9a505e293fe2a5b
Ali Craigmile <ali@craigmile.com>
2015-02-26 18:26:21 +0000

    added /relationships/by/object endpoint

--- app/app.rb
+++ app/app.rb
@@ -147,6 +147,29 @@ get '/relationships/?' do
   end
 end
 
+get '/relationships/by/object/?' do
+  @relationships = Relationship.where(:object => params[:uri])
+  @by = 'object'
+  @uri = params[:uri]
+    
+  respond_to do |wants|
+    wants.json  {
+      content_type :json
+      {:relationships => @relationships}.to_json
+    }
+    wants.xml   {
+      content_type :xml
+      builder :relationships }
+    wants.html   {
+      content_type :html
+      erb :relationships  }
+    wants.other { 
+      content_type 'text/plain'
+      error 406, "Not Acceptable" 
+    }
+  end
+end
+
 get '/relationships/by/subject/?' do
   @relationships = Relationship.where(:subject => params[:uri])
   @by = 'subject'
