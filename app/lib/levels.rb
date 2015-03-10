65481b6c6ea93d5c3be7dd75a77ef42bbe3ec46a
Ali Craigmile <ali@craigmile.com>
2015-03-10 15:31:52 +0000

    Added description field to level Added /levels/:id route Added XML, HTML and JS views to this route

--- app/lib/levels.rb
+++ app/lib/levels.rb
@@ -1,19 +1,25 @@
 class Level
-  attr_accessor :label, :id
+  attr_accessor :label, :id, :description
 
-  def initialize (label, id = nil)
+  def initialize (label, id = nil, description = nil)
     @label = label
     @id = id
+    @description = description
   end
-  
+
+  def self.find_by (options={})
+    self.all().find { |level| level.id == options[:id] }
+  end
+
+    
   def self.all ()
     [
-      Level.new('KS1','z3g4d2p'), 
+      Level.new('KS1','z3g4d2p', ''), 
       Level.new('Early and 1st level', 'zgckjxs'),
-      Level.new('KS2','zbr9wmn'),
+      Level.new('KS2','zbr9wmn', 'Key Stage 2 is a phase of primary education for pupils aged 7 to 11 in England and Wales, or 8 to 11 in Northern Ireland.'),
       Level.new('2nd Level', 'zr48q6f'),
       Level.new('3rd Level', 'zy4qn39'),
-      Level.new('4th Level', 'zvk2fg8'),
+      Level.new('4th Level', 'zvk2fg8', 'Fourth Level is a phase of education in Scotland, generally for pupils aged 11 to 15, in their first three years of secondary school.'),
       Level.new('CA3', 'zh6vr82'),
       Level.new('KS3','z4kw2hv'),
       Level.new('GCSE','z98jmp3'),
