7458f6526d41dad9cdba4c7362f5b3b40c32423c
Ali Craigmile <ali@craigmile.com>
2015-03-10 00:22:00 +0000

    Now calling API on each browser tab refresh to check tag status.

--- chrome-extension/eventPage.js
+++ chrome-extension/eventPage.js
@@ -1,39 +1,68 @@
+var API_URL = 'http://localhost:9292/'
+	
+function isTagged(uri, callback, errorCallback) {
+  var getUrl = API_URL + '/relationships/by/subject?uri='  + encodeURI(uri)
+  var x = new XMLHttpRequest();
+  x.open('GET', getUrl);
+  x.setRequestHeader ("Accept", "application/json");
+  x.responseType = 'json';
+  x.onload = function() {
+    // Parse and process the response from Google Image Search.
+    var response = x.response;
+    
+    if (!response || !response.relationships[0]) {
+        console.log('bad data')
+      errorCallback('No (or bad) response from relationships API!');
+      return;
+    }
+
+    var relationship = response.relationships[0];
+
+    var id = relationship.id;
+    console.assert(
+        typeof !isNaN(id),
+        'Unexpected respose from the Relationships API!');
+    console.log('callback: ' + id)
+    callback(id);
+  };
+  x.onerror = function(err) {
+    errorCallback('Network error.' + err.description);
+  };
+  x.send();
+}
+
+
 chrome.tabs.onActivated.addListener(function(e) {
 	var tabId = e.tabId;
 	console.log('tab ' + tabId + ' activated.'); 
 	chrome.tabs.get(tabId, function(tab) {
-		var title = tab.title;
+		var url = tab.url;
 		
-		if (title.indexOf('BBC') > -1) {
-			console.log('bbc page');
-			chrome.browserAction.setIcon({path:"icon-tagged.png"});
-		} else {
-			console.log('non bbc page');
+		isTagged(url,function(id) {
+			chrome.browserAction.setIcon({path:"icon-tagged.png"});	
+		}, function(err) {
 			chrome.browserAction.setIcon({path:"icon-tag.png"});
-		}	
-		
+		});
+				
 	});
 });
 
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
 	console.log('tab title: ' + tab.title); 	
 	console.log('tab url: ' + tab.url);
-	var title = tab.title;
+	var url = tab.url;
 	
-	if (title.indexOf('BBC') > -1) {
-		console.log('bbc page');
-		chrome.browserAction.setIcon({path:"icon-tagged.png"});
-	} else {
-		console.log('non bbc page');
+	isTagged(url,function(id) {
+		chrome.browserAction.setIcon({path:"icon-tagged.png"});	
+	}, function(err) {
 		chrome.browserAction.setIcon({path:"icon-tag.png"});
-	}
+	});
+	
 });
 
 chrome.tabs.onCreated.addListener(function(e) {         
 	var tabId = e.tabId;
 	
 	console.log('tab ' + tabId + ' created - but it\'s probably too early to know anything about it yet.'); 
-	
-	
 });
 
