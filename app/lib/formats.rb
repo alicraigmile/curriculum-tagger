c7362076b4c74b350dc00272265494824f616e86
Ali Craigmile <ali@craigmile.com>
2015-03-17 18:29:34 +0000

    Added /formats and /formats/:id endpoints - hardcoded data for now.

---
+++ app/lib/formats.rb
@@ -0,0 +1,40 @@
+class Format
+  attr_accessor :label, :id, :description
+
+  def initialize (label, id = nil, description = nil)
+    @label = label
+    @id = id
+    @description = description
+  end
+
+
+  # A decorator should return this ideally - that way, we'd ensure the hostname featured too
+  def uri
+    '/formats/' + @id
+  end
+
+  
+  def self.find_by (options={})
+    self.all().find { |format| format.id == options[:id] }
+  end
+
+    
+  def self.all ()
+    [
+      Format.new('Index','index'),
+      Format.new('Revision bite','revision-bite'),
+      Format.new('Activity','activity'),
+      Format.new('Video','video'),
+      Format.new('Audio clip','audio'),
+      Format.new('Quiz','quiz'),
+      Format.new('Study guide','study-guide'),
+      Format.new('Worksheet','worksheet'),
+      Format.new('Guide','guide'),
+      Format.new('Programme','programme'),
+      Format.new('Other','other')
+    ]
+  end
+  
+end
+
+
